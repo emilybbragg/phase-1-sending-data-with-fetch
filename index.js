5081e5e703491ff6247aa5d04e8295cf90772c1a
Emily <emilybribragg@gmail.com>
2021-12-13 13:07:10 -0500

    commit tree 863699895b8d2dc18c9f2dfab963fe18b50db633

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+    const configureObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+    },
+        body:JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    };
+    return fetch('http://localhost:3000/users', configureObject)
+    .then(response => response.json()) 
+    .then(body => addNewInfo(body.id))
+    .catch(error => {
+        let errorMessage=document.createElement("errorMessage")
+        errorMessage.innerHTML='Unauthorized Access';
+        document.body.append(errorMessage)
+    })
+}
+
+
+function addNewInfo(newID){
+    document.getElementsByTagName('body')[0].innerHTML = newID;
+}
